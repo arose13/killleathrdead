66e97da79daf1570a1dcaa2680de40f3bdac9e3a
AnthonyRose <AnthonyRose@AnthonyRose-HP>
2014-02-18 16:25:56 -0500

    QuoteView is now ready for coding

--- src/co/leathr/app/views/QuoteView.java
+++ src/co/leathr/app/views/QuoteView.java
@@ -1,12 +1,19 @@
 package co.leathr.app.views;
 
+import android.os.Bundle;
+
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.plus.model.people.PersonBuffer;
 
 import co.leathr.app.activities.BaseActivity;
 
 public class QuoteView extends BaseActivity {
-
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+	
 	@Override
 	public void onPeopleLoaded(ConnectionResult status, PersonBuffer personBuffer, String nextPageToken) {
 		assignUserID(status, personBuffer);
