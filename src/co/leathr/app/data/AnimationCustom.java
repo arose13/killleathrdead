288e38edf8188ab571145d000987230a13bdbd68
arose13 <anthony_rose12@hotmail.com>
2014-03-07 16:41:43 -0500

    New AnimationCustom.class methods

--- src/co/leathr/app/data/AnimationCustom.java
+++ src/co/leathr/app/data/AnimationCustom.java
@@ -1,13 +1,19 @@
 package co.leathr.app.data;
 
 import android.view.animation.Animation;
+import android.view.animation.Interpolator;
 import android.view.animation.LinearInterpolator;
 import android.view.animation.TranslateAnimation;
 
 public class AnimationCustom {
 		
-	private long secondsToMilis(long seconds) {
+	public long secondsToMilis(long seconds) {
 		return (seconds*1000);
+	}
+	
+	public Animation delayAnimation(long delayMilis, Animation inputAnimation) {
+		inputAnimation.setStartOffset(delayMilis);
+		return inputAnimation;
 	}
 	
 	public Animation repeatAnimation(Animation inputAnimation) {
@@ -16,25 +22,47 @@ public class AnimationCustom {
 		return inputAnimation;
 	}
 	
-	public Animation fromRightToLeftAnimation(long seconds) {
+	public Animation fromRightToLeftAnimation(long milis, Interpolator animationInterpolator) {
 		Animation fromRightToLeft = new TranslateAnimation(
 				Animation.RELATIVE_TO_PARENT, +1.0f,
 				Animation.RELATIVE_TO_PARENT, -1.0f,
 				Animation.RELATIVE_TO_PARENT, 0.0f,
 				Animation.RELATIVE_TO_PARENT, 0.0f);
-		fromRightToLeft.setDuration(secondsToMilis(seconds));
-		fromRightToLeft.setInterpolator(new LinearInterpolator());
+		fromRightToLeft.setDuration(milis);
+		fromRightToLeft.setInterpolator(animationInterpolator);
 		return fromRightToLeft;
 	}
 	
-	public Animation fromLeftToRightAnimation(long seconds) {
+	public Animation fromLeftToRightAnimation(long milis, Interpolator animationInterpolator) {
 		Animation fromLeftToRight = new TranslateAnimation(
 				Animation.RELATIVE_TO_PARENT, -1.0f, 
 				Animation.RELATIVE_TO_PARENT, +1.0f, 
 				Animation.RELATIVE_TO_PARENT, 0.0f, 
 				Animation.RELATIVE_TO_PARENT, 0.0f);
-		fromLeftToRight.setDuration(secondsToMilis(seconds));
+		fromLeftToRight.setDuration(milis);
 		fromLeftToRight.setInterpolator(new LinearInterpolator());
 		return fromLeftToRight;
+	}
+	
+	public Animation inFromTopAnimation(long milis, Interpolator animationInterpolator) {
+		Animation inFromTop = new TranslateAnimation(
+				Animation.RELATIVE_TO_PARENT, 0.0f, 
+				Animation.RELATIVE_TO_PARENT, 0.0f, 
+				Animation.RELATIVE_TO_PARENT, -1.0f, 
+				Animation.RELATIVE_TO_PARENT, 0.0f);
+		inFromTop.setDuration(milis);
+		inFromTop.setInterpolator(animationInterpolator);
+		return inFromTop;
+	}
+	
+	public Animation inFromBottomAnimation(long milis, Interpolator animationInterpolator) {
+		Animation inFromBottom = new TranslateAnimation(
+				Animation.RELATIVE_TO_PARENT, 0.0f,
+				Animation.RELATIVE_TO_PARENT, 0.0f,
+				Animation.RELATIVE_TO_PARENT, 1.0f,
+				Animation.RELATIVE_TO_PARENT, 0.0f);
+		inFromBottom.setDuration(milis);
+		inFromBottom.setInterpolator(animationInterpolator);
+		return inFromBottom;
 	}
 }
